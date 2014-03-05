906b1b2cd922270b4b37bde4f91a32efb1c096e6
Alexander Björkén <4242687+bizzno@users.noreply.github.com>
2014-03-05 06:10:34 +0100

    Fix bug in product_scraper.py

---
+++ src/scrapers/product_scraper.py
@@ -0,0 +1,18 @@
+"""Product scraper for e-commerce sites"""
+import requests
+from bs4 import BeautifulSoup
+
+class ProductScraper:
+    def __init__(self, base_url):
+        self.base_url = base_url
+
+    def scrape_products(self):
+        """Scrape product listings"""
+        products = []
+        # TODO: Implement scraping logic
+        return products
+
+    def extract_price(self, element):
+        """Extract price from element"""
+        return 0.0
+Update 5 on 2014-03-05 06:10:34
