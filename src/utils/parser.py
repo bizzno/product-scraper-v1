e30a1e2471b9f0ba09a8e5a2ed0798788fe2a493
Alexander Björkén <4242687+bizzno@users.noreply.github.com>
2014-03-05 21:14:52 +0100

    Update parser.py

---
+++ src/utils/parser.py
@@ -0,0 +1,11 @@
+"""HTML parsing utilities"""
+from bs4 import BeautifulSoup
+
+def parse_html(html_content):
+    """Parse HTML content"""
+    return BeautifulSoup(html_content, 'html.parser')
+
+def extract_text(element):
+    """Extract text from element"""
+    return element.get_text(strip=True)
+Update 3 on 2014-03-05 21:14:52
